8dcae5d673f99af53e843a8fff83407c39326b79
Pamela Lim <2ppaamm@users.noreply.github.com>
2024-05-30 03:05:14 +0800

    Add files via upload

---
+++ custom-addons/custom_theme/__init__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Theme',
+    'description': 'Custom CSS for backend theme',
+    'category': 'Theme/Backend',
+    'version': '1.0',
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml',
+    ],
+}
